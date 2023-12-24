8049c99971cd15579082ffc7b6e2874509797a98
Abhijith Gangadharan <abhisivaganga10@gmail.com>
2023-12-24 13:04:15 +0530

    change

--- bubble_sort.c
+++ bubble_sort.c
@@ -9,11 +9,11 @@ void main (){
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++){
-        for(j=0;j<n-i-1;j++){
+        for(j=1;j<n-1-i;j++){
             if(a[j]>a[j+1]){
-                a[temp]=a[j];
+                temp=a[j];
                 a[j]=a[j+1];
-                a[j+1]=a[temp];
+                a[j+1]=temp;
             }
         }
         
